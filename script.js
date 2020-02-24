c2a9592782eb8ffd0ab90433102f8bace422a7cc
juma-359 <joeljuma08@gmail.com>
2020-02-24 15:11:25 +0300

    removed all the photos and icons from the folder i donloaded them with this will change the location of the images.

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-// $(document).ready(function(){
-//     $(".icon-hide").click(function(){
-//         $("#design-show").toogle()
-//         $("#elani-show").toogle()
-//     })
-// })
-var documents = document
 $(document).ready(function(){
-    $(".icon-hide").click(function(){
-        $("design-show").hide();
-    });
-    $(".icon-hide").click(function(){
-        $("#design-show").show();
+    $("#des").click(function(){
+        $(".click").toggle();
+        $(".type").toggle();
     });
 });
+
+$(document).ready(function(){
+    $("#dev").click(function(){
+    $(".click1").toggle();
+    $(".type1").toggle();
+});
+});
+
+$(document).ready(function(){
+    $("#prod").click(function(){
+        $(".click2").toggle();
+        $(".type2").toggle();
+    });
+});
+
+// var name = prompt("Please enter your name");
+
+// if (person == null || person == "") {
+//   txt = "User cancelled the prompt.";
+// } else {
+//   txt = "Hello " + person + "! How are you today?";
+// }
